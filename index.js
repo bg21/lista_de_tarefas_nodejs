41756015f81fb2129fa8c5f28f95ea0b820048a0
devbg21 <juhcosta23@gmail.com>
2021-03-26 16:20:52 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser'); 
+
 const path = require('path');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
@@ -14,7 +20,13 @@ let tarefas = ['Arrumar o quarto -_-', 'outra coisa', 'mais outra tarefa'];
 
 app.get("/", function(req, res){
     res.render('index.ejs', {tarefas: tarefas});
+    
 });
+
+app.post("/", function(req, res){
+    tarefas.push(req.body.tarefa);
+    res.render('index.ejs', {tarefas: tarefas});
+}); 
 
 app.get("/deletar/:id", function(req, res){
     tarefas = tarefas.filter(function(val, index){
